cecc92b3b630c2086299591cfa787de5e7f782d5
bipinemp <bhandaribipin3.39@gmail.com>
2024-01-21 23:25:42 +0545

    changes

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["https://bipinchatapp.vercel.app"],
     },
 });
 app.get("/", (req, res) => res.send("Hello from server"));
